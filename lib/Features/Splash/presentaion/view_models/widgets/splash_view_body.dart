3036c71154b4762eacc5cbc97bff5c735ffad329
Aya-Dahlan <ayaalidahlan@gmail.com>
2024-05-20 18:01:52 +0300

    add animation to splash

--- lib/Features/Splash/presentaion/view_models/widgets/splash_view_body.dart
+++ lib/Features/Splash/presentaion/view_models/widgets/splash_view_body.dart
@@ -1,10 +1,44 @@
+import 'package:bookly_app/Features/Splash/presentaion/view_models/widgets/sliding_text.dart';
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidinAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        seconds: 1,
+      ),
+    );
+
+    slidinAnimation = Tween<Offset>(begin: const Offset(0, 10), end: Offset.zero)
+        .animate(animationController);
+        animationController.forward();
+      
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+     animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,11 +46,14 @@ class SplashViewBody extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        SvgPicture.asset(AssetsData.logo, width: 220.w,), 
-         SizedBox(
+        SvgPicture.asset(
+          AssetsData.logo,
+          width: 220.w,
+        ),
+        SizedBox(
           height: 10.h,
         ),
-        const Text('Read Free Books', textAlign: TextAlign.center,),
+        SlidingText(slidinAnimation: slidinAnimation),
       ],
     );
   }
